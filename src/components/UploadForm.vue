9985c9635a65c95285bc9ea71197f803c114d34e
Joshua-7 <blurrydude@hotmail.com>
2022-04-10 21:06:00 -0500

    Added upload

---
+++ src/components/UploadForm.vue
@@ -0,0 +1,34 @@
+<template>
+   <form method="POST" enctype="multipart/form-data" @submit.prevent="uploadPhoto">
+  <label for="photo">Photo:</label>
+  <input type="file"  id="photo" name="photo" accept="image/*"><br><br>
+  <label for="description">Description:</label>
+  <input type="textarea" id="description"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+</template>
+
+<script>
+export default {
+    data() {
+    return {};
+    },
+    methods:
+    {
+        uploadPhoto()
+        {
+            fetch("/api/upload", {method: 'POST'})
+                .then(function (response) {
+                    return response.json();
+                })
+            .then(function (data) {
+                // display a success message
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+    }
+}
+</script>
---
+++ src/views/UploadView.vue
@@ -0,0 +1,16 @@
+<template>
+ <div class="Upload container">
+ <h2>Upload</h2>
+ </div>
+ <UploadForm />
+ </template>
+
+<script>
+import UploadForm from '@/components/UploadForm.vue'
+export default {
+ data() {
+ return {};
+ },
+ components: { UploadForm }
+};
+</script>
